938449da2cf5f5b8383ad94ee8c52722ad58ce51
Gbenga Oshinaga <gbenga.oshinaga@signalalliance.com>
2017-11-02 20:20:14 +0000

    Edited app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import Express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routes/index';
+import db from './models/index.js';
 
 // Set up the express app
 const app = new Express();
@@ -9,7 +10,7 @@ const app = new Express();
 // Log requests to the console.
 app.use(logger('dev'));
 
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
+// Parse incoming requests data (https://gC:\Users\gbenga.oshinaga\Documents\Andela\more-recipes\more-recipes\node_modules\babylon\lib\inithub.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -21,6 +22,12 @@ app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the More-Recipes API Home.',
 }));
 
-app.listen(process.env.PORT || 8000);
+const port = process.env.PORT || 8000;
+
+db.sequelize.sync().then(() => {
+	app.listen(port, () => {
+	  console.log(`listening to port ${port}`);
+	});
+});
 
 export default app;
