caafcc289a7de2c929d54ea28f4db19cc1a1b1ff
Gbenga Oshinaga <gbenga.oshinaga@signalalliance.com>
2017-11-02 05:01:51 +0000

    Added authentication with jwt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 import jsend from 'jsend';
 import RecipeValidator from '../validator/RecipeValidator';
 import UserValidator from '../validator/UsersValidator';
+import AuthValidator from '../validator/AuthValidator';
 import RecipesController from '../controllers/RecipesController';
 import UserController from '../controllers/UserController';
 
@@ -9,10 +10,10 @@ const recipes = new RecipesController();
 export default (app) => {
   app.use(jsend.middleware);
   app.get('/api/v1/recipes/', (req, res) => { recipes.getRecipes(req, res); });
-  app.post('/api/v1/recipes/', RecipeValidator.validateRecipe, (req, res) => { recipes.addRecipe(req, res); });
-  app.put('/api/v1/recipes/:id', RecipeValidator.validateID, (req, res) => { recipes.modifyRecipe(req, res); });
-  app.delete('/api/v1/recipes/:id', RecipeValidator.validateID, (req, res) => { recipes.deleteRecipe(req, res); });
-  app.post('/api/v1/recipes/:id/reviews', RecipeValidator.validateID, (req, res) => { recipes.addReview(req, res); });
+  app.post('/api/v1/recipes/', RecipeValidator.validateRecipe, AuthValidator.authenticate, (req, res) => { recipes.addRecipe(req, res); });
+  app.put('/api/v1/recipes/:id', RecipeValidator.validateID, AuthValidator.authenticate, (req, res) => { recipes.modifyRecipe(req, res); });
+  app.delete('/api/v1/recipes/:id', RecipeValidator.validateID, AuthValidator.authenticate, (req, res) => { recipes.deleteRecipe(req, res); });
+  app.post('/api/v1/recipes/:id/reviews', RecipeValidator.validateID, AuthValidator.authenticate, (req, res) => { recipes.addReview(req, res); });
   app.post('/api/v1/users/signup', UserValidator.validateSignUp, (req, res) => { UserController.signup(req, res); });
   app.post('/api/v1/users/signin', (req, res) => { UserController.signin(req, res); });
 
---
+++ server/validator/AuthValidator.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+/**
+ * Class for authenticating through access tokens
+ */
+export default class AuthValidator{
+    
+    /**
+     * Authenticates user through access token
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
+    static authenticate(req, res, next){
+        const accessToken = req.body.accessToken;
+
+        try {
+            const user = jwt.verify(accessToken, 'mysecret');
+            if(user.email){
+                console.log(user.email);
+                req.user = user;
+                return next();
+            }
+            res.status(400).jsend.fail('Invalid token');
+        } catch (error) {
+            res.status(401).jsend.error('Authentication is required to perform this request');
+        }
+    }
+}
