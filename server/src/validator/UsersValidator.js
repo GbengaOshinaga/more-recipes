75dad74e7b43061b2e59445c2faf99fc9bd81c27
Gbenga Oshinaga <gbenga.oshinaga@signalalliance.com>
2017-11-03 03:48:02 +0000

    Added sign in validation

--- server/src/validator/UsersValidator.js
+++ server/src/validator/UsersValidator.js
@@ -36,4 +36,29 @@ export default class UsersValidator {
     }
     next();
   }
+
+  /**
+   * Validates sign in
+   * @param {*} req 
+   * @param {*} res 
+   * @param {*} next 
+   */
+  static validateSignIn(req, res, next){
+    let messages = [];
+
+    if (!req.body.email) {
+      messages.push('Email is required');
+    }
+    if (!req.body.password) {
+      messages.push('Password is required');
+    }
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(req.body.email)) {
+      messages.push('Email Address is not valid');
+    }
+    if (messages.length > 0) {
+      return res.status(400).jsend.fail({ errors: messages });
+    }
+    next();
+  }
 }
