75ef9bce3122605c7a2a56cfe71f074481bc78ab
Gbenga Oshinaga <gbenga.oshinaga@signalalliance.com>
2017-11-17 22:47:45 +0100

    Added more tests

--- server/src/test/appTest.js
+++ server/src/test/appTest.js
@@ -4,8 +4,9 @@ import app from '../app';
 
 process.env.NODE_ENV = 'test';
 
-const expect = chai.expect;
-let token;
+const { expect } = chai;
+let firstToken;
+let secondToken;
 chai.use(chaiHttp);
 
 describe('/api/v1/recipes', () => {
@@ -38,6 +39,26 @@ describe('/api/v1/recipes', () => {
       });
   });
 
+  it('it should sign up a user and return a token', (done) => {
+    chai.request(app)
+      .post('/api/v1/users/signup')
+      .send({
+        firstName: 'James',
+        lastName: 'Gorden',
+        email: 'gorden@email.com',
+        password: 'iamjamesgorden',
+        confirmPassword: 'iamjamesgorden'
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.data.user.firstName).to.equal('James');
+        expect(res.body.data.token).to.be.a('string');
+        secondToken = res.body.data.token;
+        done();
+      });
+  });
+
   it('it should return errors when signing up without appropriate keys', (done) => {
     chai.request(app)
       .post('/api/v1/users/signup')
@@ -62,7 +83,7 @@ describe('/api/v1/recipes', () => {
         expect(res).to.have.status(200);
         expect(res.body.status).to.equal('success');
         expect(res.body.data.token).to.be.a('string');
-        token = res.body.data.token;
+        firstToken = res.body.data.token;
         done();
       });
   });
@@ -83,7 +104,7 @@ describe('/api/v1/recipes', () => {
   it('it should post recipe', (done) => {
     chai.request(app)
       .post('/api/v1/recipes')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .send({
         name: 'Fried Chicken with sauce',
         description: 'It\'s fried chicken, what more do you want?',
@@ -96,17 +117,32 @@ describe('/api/v1/recipes', () => {
       });
   });
 
+  it('it should post recipe', (done) => {
+    chai.request(app)
+      .post('/api/v1/recipes')
+      .set('Access-Token', firstToken)
+      .send({
+        name: 'Toast bread with sauce',
+        description: 'It\'s toast bread, with sauce, yeah, it might be disgusting',
+        ingredients: 'bread,sauce'
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.data.name).to.equal('Fried Chicken with sauce');
+        done();
+      });
+  });
+
   it('it should edit recipe', (done) => {
     chai.request(app)
       .put('/api/v1/recipes/1')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .send({
         name: 'Editing recipe name',
         description: 'It\'s fried chicken, what more do you want?',
         ingredients: 'chicken,sauce'
       })
       .end((err, res) => {
-        console.log(token);
         expect(res).to.have.status(200);
         expect(res.body.data.name).to.equal('Editing recipe name');
         done();
@@ -116,7 +152,7 @@ describe('/api/v1/recipes', () => {
   it('it should send error if recipe id does not exist', (done) => {
     chai.request(app)
       .put('/api/v1/recipes/92')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .send({
         name: 'Editing recipe name',
         description: 'It\'s fried chicken, what more do you want?',
@@ -132,7 +168,7 @@ describe('/api/v1/recipes', () => {
   it.skip('it should send error if user is not authorized to edit recipe', (done) => {
     chai.request(app)
       .put('/api/v1/recipes/1')
-      .set('Access-Token', token)
+      .set('Access-Token', secondToken)
       .send({
         name: 'Editing recipe name',
         description: 'It\'s fried chicken, what more do you want?',
@@ -149,7 +185,7 @@ describe('/api/v1/recipes', () => {
   it('it should return errors when posting recipe without appropriate keys', (done) => {
     chai.request(app)
       .post('/api/v1/recipes')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .send({
       })
       .end((err, res) => {
@@ -182,10 +218,20 @@ describe('/api/v1/recipes', () => {
       });
   });
 
+  it('it should get all recipes and sort them by upvotes', (done) => {
+    chai.request(app)
+      .get('/api/v1/recipes?sort=upvotes&order=desc')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
+
   it('it should get all recipes created by user', (done) => {
     chai.request(app)
       .get('/api/v1/users/recipes')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.data).to.be.an('array');
@@ -196,7 +242,7 @@ describe('/api/v1/recipes', () => {
   it('it should add user favourites recipes', (done) => {
     chai.request(app)
       .post('/api/v1/users/recipes/1/favourites')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.data.message).to.equal('Favourite added');
@@ -207,7 +253,7 @@ describe('/api/v1/recipes', () => {
   it('it should get user favourites recipes', (done) => {
     chai.request(app)
       .get('/api/v1/users/recipes/favourites')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.data).to.be.an('array');
@@ -219,7 +265,7 @@ describe('/api/v1/recipes', () => {
   it('it should delete user favourite recipe', (done) => {
     chai.request(app)
       .del('/api/v1/users/recipes/1/favourites')
-      .set('Access-Token', token)
+      .set('Access-Token', firstToken)
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.data.message).to.equal('Favourite deleted');
