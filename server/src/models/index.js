386348012c0396df7e03c57fa500f4717f94bc88
GbengaOshinaga <goshinaga@yahoo.com>
2017-11-26 14:17:41 +0100

    Fixing issues for travis ci

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 import configJson from '../config/config';
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'test';
 const config = configJson[env];
 const db = {};
 console.log(env);
